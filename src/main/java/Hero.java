9f7a2246438eb26da238c6a9fc711e797d9329c3
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-23 15:08:37 +0300

    Crete a constructor Hero. Create a test to let us know we can create an instance of Hero.

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,5 @@
 public class Hero {
+public Hero (String name, int age, String specialPowers, String weakness){
 
+}
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void newHeroObjectGetsCorrectlyCreated_true() throws Exception {
+        Hero hero= new Hero("perpy", 30, "shapeshifting", "envy");
+        assertEquals(true, hero instanceof Hero);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
